2a19ed4b191d7e8d4121035341e109501fc38b6b
Ganesh Umashankar Nashikar <Ganesh_Nashikar@ad.infosys.com>
2018-08-15 23:06:07 +0530

    Spring Sample project

---
+++ Telecom/src/main/java/com/telstra/TelecomController.java
@@ -0,0 +1,22 @@
+package com.telstra;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TelecomController {
+	String message = "Welcome to Telstra!";
+ 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("entered in controller.");
+ 
+		ModelAndView mview = new ModelAndView("helloTelstra");
+		mview.addObject("message", message);
+		mview.addObject("name", name);
+		return mview;
+	}
+}
